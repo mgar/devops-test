2327d1389baafa2f92c99c35b4d46952059e9a64
Miguel Angel Garcia <mgarcia.inf@gmail.com>
2015-10-28 22:36:08 +0100

    add docker-compose.yml

--- php-fpm/src/index.php
+++ php-fpm/src/index.php
@@ -1,1 +1,44 @@
-hola mundo
+<HTML>
+<BODY>
+<?php
+
+$servername = getenv('MYSQL_PORT_3306_TCP_ADDR');
+$username = getenv('MYSQL_ENV_MYSQL_USER');
+$password = getenv('MYSQL_PASS');
+$dbname = "teamcmp";
+$table = "visitors";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection error: " . $conn->connect_error);
+}
+
+$sql = "CREATE TABLE $table ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, ip varchar(30) NOT NULL)";
+$check_table = "SHOW TABLES LIKE $table";
+
+if($conn->query($check_table) == 0) {
+  $conn->query($sql);
+}
+
+$sql = "INSERT INTO $table (ip) VALUES ('$_SERVER[REMOTE_ADDR]')";
+
+if ($conn->query($sql) === TRUE) {
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "SELECT * FROM $table ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+
+$visitor = $result->fetch_assoc();
+
+echo "<br><p align='center'>You are visitor number $visitor[id] with the IP address: $visitor[ip] </p>";
+
+$conn->close();
+
+?>
+
+</BODY>
+</HTML>
